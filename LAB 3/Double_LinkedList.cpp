1f61937de55c947a2bf466371da81648fdc77aef
pnattt <PUTERI_22010207@utp.edu.my>
2025-06-17 20:39:34 +0800

    Add files via upload

---
+++ LAB 3/Double_LinkedList.cpp
@@ -0,0 +1,81 @@
+/*
+id: 22010207
+name: puteri natasha
+group: g3
+lab: L3
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Node {
+    string name;
+    Node* next_pointer; // Pointer to the next Node
+    Node* prev_pointer; // Pointer to the previous Node
+
+    // Constructor to initialize Node
+    Node(string n) : name(n), next_pointer(nullptr), prev_pointer(nullptr) {}
+};
+
+class Linkedlist {
+    Node* head; // Pointer to the head of the list
+    Node* tail; // Pointer to the tail of the list
+
+public:
+    Linkedlist() : head(nullptr), tail(nullptr) {} // Constructor to initialize head
+
+    void insert_node(Node* node) {
+        if (head == nullptr) {
+            head = tail = node; // If list is empty, set head to the new node
+        } else {
+            // Append at the end
+            tail->next_pointer = node;
+            node->prev_pointer = tail;
+            tail = node;
+        }
+    }
+
+    void display_list() {
+        Node* currNode = head;
+        while (currNode != nullptr) {
+            cout << currNode->name << " "; // Print the name of the current node
+            currNode = currNode->next_pointer; // Move to the next node
+        }
+    }
+    
+    void display_list_reverse() {
+        Node* currNode = tail;
+        while (currNode != nullptr) {
+            cout << currNode->name << " ";
+            currNode = currNode->prev_pointer;
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    // Create nodes
+    Node* node1 = new Node("Aiman");
+    Node* node2 = new Node("Alisha");
+    Node* node3 = new Node("Husin");
+
+    Linkedlist std_names;
+
+    std_names.insert_node(node1);
+    std_names.insert_node(node2);
+    std_names.insert_node(node3);
+
+    cout << "Forward traversal: ";
+    std_names.display_list(); // Display the list forward
+    
+    cout << endl;
+
+    cout << "Reverse traversal: ";
+    std_names.display_list_reverse(); // Display the list backward
+
+
+
+    return 0;
+}
---
+++ LAB 3/Singly_LinkedList.cpp
@@ -0,0 +1,67 @@
+/*
+id: 22010207
+name: puteri natasha
+group: g3
+lab: L3
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Node {
+    string name;
+    Node* next_pointer; // Pointer to the next Node
+
+    // Constructor to initialize Node
+    Node(string n) : name(n), next_pointer(nullptr) {}
+};
+
+class Linkedlist {
+    Node* head; // Pointer to the head of the list
+
+public:
+    Linkedlist() : head(nullptr) {} // Constructor to initialize head
+
+    void insert_node(Node* node) {
+        if (head == nullptr) {
+            head = node; // If list is empty, set head to the new node
+        } else {
+            Node* currNode = head;
+            while (currNode->next_pointer != nullptr) {
+                currNode = currNode->next_pointer; // Traverse to the end of the list
+            }
+            currNode->next_pointer = node; // Link the new node
+        }
+    }
+
+    void display_list() {
+        Node* currNode = head;
+        while (currNode != nullptr) {
+            cout << currNode->name << " "; // Print the name of the current node
+            currNode = currNode->next_pointer; // Move to the next node
+        }
+        cout << endl; // New line after displaying the list
+    }
+
+    // You can implement delete_node if needed
+};
+
+int main() {
+    // Create nodes
+    Node* node1 = new Node("Aiman");
+    Node* node2 = new Node("Alisha");
+    Node* node3 = new Node("Husin");
+
+    Linkedlist std_names;
+
+    std_names.insert_node(node1);
+    std_names.insert_node(node2);
+    std_names.insert_node(node3);
+
+    std_names.display_list(); // Display the list
+
+
+    return 0;
+}
